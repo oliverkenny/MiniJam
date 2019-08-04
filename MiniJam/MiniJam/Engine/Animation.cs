53966e65a4a6ea7618c15f95f37873cf869dc458
oliverkenny <oliver.j.kenny@gmail.com>
2019-08-04 15:07:22 +0100

    Working animator

--- MiniJam/MiniJam/Engine/Animation.cs
+++ MiniJam/MiniJam/Engine/Animation.cs
@@ -10,25 +10,42 @@ namespace MiniJam.Engine
 {
 	public class Animation
 	{
-		const float ANIMATIONSPEED = 4f;
+		const float ANIMATIONSPEED = 4f; 
 		private Texture2D[] Frames { get; set; }
 		private int CurrentFrame { get; set; }
 		public float AnimationSpeed { get; set; }
+		public float SecondCounter { get; set; }
 
-		public Animation(GameTime gameTime, Texture2D[] frames, float? animationSpeed = null)
+		public Animation(GameTime gameTime, Texture2D[] frames, float? animationSpeed = null, int? currentFrame = null)
 		{
 			this.Frames = frames;
 			this.AnimationSpeed = animationSpeed ?? ANIMATIONSPEED * (float)gameTime.ElapsedGameTime.TotalSeconds;
+			this.CurrentFrame = currentFrame ?? 0;
 		}
 
-		public void Update()
+		public void Update(GameTime gameTime)
 		{
+			SecondCounter += gameTime.ElapsedGameTime.Seconds;
 
+			// If frame needs iterating
+			if (SecondCounter > 60 / ANIMATIONSPEED)
+			{
+				// Iterate frame
+				CurrentFrame++;
+
+				// If at last frame
+				if (CurrentFrame > Frames.Length) {
+					CurrentFrame = 0;
+				}
+
+				// Reset frame counter
+				SecondCounter -= 60 / ANIMATIONSPEED;
+			}
 		}
 
-		public void Draw()
+		public Texture2D GetFrame()
 		{
-
+			return Frames[CurrentFrame];
 		}
 	}
 }
