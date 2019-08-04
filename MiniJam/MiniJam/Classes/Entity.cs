ab4fdefd525998eb3c8877b6c5395d40df580d45
oliverkenny <oliver.j.kenny@gmail.com>
2019-08-04 14:11:26 +0100

    Added brick texture and started entity

---
+++ MiniJam/MiniJam/Classes/Entity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniJam.Classes
+{
+	public abstract class Entity
+	{
+		public string Name { get; set; }
+		public Vector2 Position { get; set; }
+
+		public abstract void Update();
+		public abstract void Draw();
+	}
+}
